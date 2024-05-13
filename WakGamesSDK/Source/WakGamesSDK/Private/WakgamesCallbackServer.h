f4365689c958fb084ca45449998886cfc4ed208f
re.t <apeach5@kakao.com>
2024-05-13 14:01:11 +0900

    🚧 chore: callback 작성을 위한 사전 코드 기초적 작성 (빌드 성공 상태로 수정함)
    (cherry picked from commit 830e140eea3d83d89b32f2124f9d3d47296c07f5)

---
+++ WakGamesSDK/Source/WakGamesSDK/Private/WakgamesCallbackServer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "WakgamesCallbackServer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UWakgamesCallbackServer : public UObject
+{
+	GENERATED_BODY()
+	
+};
